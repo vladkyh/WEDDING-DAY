a12900b83995710b12e3439fc393832e438dced9
vladkyh <mraends@gmail.com>
2025-05-21 04:18:15 +0700

    contract colors

--- src/app/page.js
+++ src/app/page.js
@@ -105,11 +105,11 @@ export default function Home() {
                 </AnimatedElement>
                 <AnimatedElementooooooo delay={0.4}>
                     <div className={styles.convert}>
-                        {/*<div className={styles.pis_qwe}>*/}
+                        <div className={styles.pis_qwe}>
                         <div className={styles.pis}>
 
                         </div>
-                        {/*</div>*/}
+                        </div>
                     </div>
                 </AnimatedElementooooooo>
                 <div className={styles.conv}></div>
